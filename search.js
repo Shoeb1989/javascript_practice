db0843e52f51e0195a2a604d390de79c293a0d8f
Shoeb1989 <psikder09@gmail.com>
2024-06-30 21:33:49 +0600

    Update New Info

--- search.js
+++ search.js
@@ -1,6 +1,13 @@
 //for find searching element
 
+// const search = 'shoeb sikder pappu'
+// const doesExist = search.includes('pappu')
+// console.log(doesExist);
+
+//or using other code,
+
 const search = 'shoeb sikder pappu'
-const doesExist = search.includes('pappu')
+const searchString = 'Pappu';
+const doesExist = search.includes(searchString);
 console.log(doesExist);
 
