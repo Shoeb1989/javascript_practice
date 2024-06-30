2731b6497b2f121cbc9ba56fd08c9309ac86f5ee
Shoeb1989 <psikder09@gmail.com>
2024-06-30 21:48:47 +0600

    Update New Info

--- split.js
+++ split.js
@@ -1,3 +1,10 @@
+// for spliting inside index
+
 const song = 'o amar bondhu go yo yo oho'
 const parts = song.split('')
+const sentence = song.split('.')
+const charts = song.split('');
 console.log(parts);
+
+const partial = song.slice(5, 8);
+console.log(partial)
