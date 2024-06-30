530a4d0b973da455bb6c89c33328959f02b01cab
Shoeb1989 <psikder09@gmail.com>
2024-06-30 21:19:25 +0600

    Update New Info

---
+++ Easy.js
@@ -0,0 +1,14 @@
+// all function convert into lower and upper
+
+const userName = 'blackPink';
+const userInput = 'blackPink';
+
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+if ( userName.toLowerCase() === userInput.toLowerCase()){
+    console.log('valid user')
+}
+else{
+    console.log('invalid user');
+}
+
