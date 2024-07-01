dd923a0b67fb5378277a3ae3acaabcbb6c9aa26c
Shoeb1989 <psikder09@gmail.com>
2024-07-01 19:48:46 +0600

    Update New Info

---
+++ max.js
@@ -0,0 +1,23 @@
+// const jim = 84;
+// const dela = 75;
+// if  (jim > dela){
+//     console.log('jim will get');
+// }
+// else {
+//     console.log('dela will get');
+// } 
+
+const jim = 84;
+const dela = 99;
+const chinku = 77;
+
+if(jim > dela && jim > chinku){
+    console.log('jim will get');
+}
+else if ( dela > jim && dela > chinku){
+    console.log('dela will get');
+}
+
+else{ 
+    console.log('chinku will get');
+}
