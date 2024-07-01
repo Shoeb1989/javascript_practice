a02e64c5253d8ca04d4baaf7509d58edfb475443
Shoeb1989 <psikder09@gmail.com>
2024-07-01 19:28:12 +0600

    Update New Info

--- math.js
+++ math.js
@@ -1,19 +1,18 @@
 // pow 
- const result = Math.pow(5, 8);
+//  const result = Math.pow(5, 8);
 //  console.log(result);
 
  const num1 = 25;
- const num2 = 22;
+ const num2 = 45;
 
- const gap = num1 - num2;
+ const gap = Math.abs(num1 - num2);
 
- if (gap < 5 ){
+if (gap < 5)
     console.log('friends');
- }
 
- else{
-    (gap > 5){
+else{
+    (gap > 5)
         console.log('not friends');
-    }
+    
  }
 
