d6e8d12b8c3f630edc8d3c310c4db564b4b0037b
Shoeb1989 <psikder09@gmail.com>
2024-07-04 20:27:45 +0600

    Update

--- use.js
+++ use.js
@@ -1,1 +1,17 @@
-n
+for (let i = 1; i <= 50; i++){
+    if ( i % 3 === 0 && i % 5 === 0){
+        console.log('footbar');
+    }
+    else if (i % 3 === 0){
+        console.log('foo');
+    }
+
+    else if (i % 5 === 0){
+        console.log('bar');
+    }
+
+
+    else {
+    console.log(i);
+    }
+}
