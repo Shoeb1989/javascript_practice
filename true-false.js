d14e7aabfe40a2178ecb4cc1c3ebb87a25bc74b1
Md Shoeb Sikder Pappu <105862964+Shoeb1989@users.noreply.github.com>
2024-08-03 22:20:08 +0600

    true-false.js

---
+++ true-false.js
@@ -0,0 +1,25 @@
+/*
+truty - true
+falst-false
+any number possitive and negetive will be truty other then 0 is false
+any strung other then empty string
+'' empty string
+null
+undefined
+*/
+
+const x = -4;
+if (x){
+    console.log('valo of x is truty');
+}
+
+else{
+    console.log('valo of x is false');
+}
+
+//optional
+// check falsy
+const y = '';
+if(!y){
+  console.log('value is falsy')
+}
