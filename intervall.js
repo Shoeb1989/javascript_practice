555eecea0bd676b050ec92cf0799e62883552faa
Md Shoeb Sikder Pappu <105862964+Shoeb1989@users.noreply.github.com>
2024-08-12 21:00:32 +0600

    new file

--- intervall.js
+++ intervall.js
@@ -1,6 +1,20 @@
-console.log(1);
-console.log(2);
-setInterval(() => {
-    console.log(3);
-}, 1000)
-console.log(4);
+// console.log(1);
+// console.log(2);
+// setInterval(() => {
+//     console.log(3);
+// }, 1000)
+// console.log(4);
+
+
+function a(){
+    console.log('a')
+    b();
+    console.log('bb')
+}
+
+function b(){
+    console.log('b')
+    console.log('aa')
+}
+
+a();
