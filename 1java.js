41034b59a3cb1cf01e46a18080b59cfc2426f90d
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2024-10-18 21:02:39 +0600

    let var const

---
+++ 1java.js
@@ -0,0 +1,10 @@
+// Dynamic Type language js
+
+// let, const & var
+
+// var : Variable can be re-declared & updated. A global scope variable.
+
+// let : Variable cannot be re-declared but can be updated. A block scope variable.
+
+// const : Variable cannot be re-declared or updated. A block scope variable.
+
