0aa93e314800913e7cd541770dfcb8d808b84446
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2024-10-18 23:22:16 +0600

    Logical operators,Comparison Operators

--- 2__Operators and Conditional Statements.js
+++ 2__Operators and Conditional Statements.js
@@ -45,4 +45,36 @@ let b = 2;
 a += 4; // a = a + 4
 console. log ("a = ", a); //9
 
+/* Comparison Operators
+
+Equal to == 
+
+Not equal to !=
+
+Equal to & type ===
+
+Not equal to & type !==
+
+>, >=, <, <=
+*/
 I
+//Comparison Operators
+let a = 5;
+let b = 2;
+
+console. log("5 = 2", a == b); //false
+
+let a = 5; //number
+let b = "5"; //string -> number
+
+console. log ("a === b", a === b) ; // False
+
+/* Logical Operators
+
+Logical AND &&
+
+Logical OR ||
+
+Logical NOT !
+
+*/
