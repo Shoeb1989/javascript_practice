67ef860b9384927a9bdb80f581e3dee722c99ff8
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2024-10-18 22:47:33 +0600

    2__Operators and Conditional Statements.js

---
+++ 2__Operators and Conditional Statements.js
@@ -0,0 +1,48 @@
+// This is a single line comment
+
+/* This is a multi-line
+comment. */
+
+/* +, -,*, I
+
+· Modulus
+
+. Exponentiation
+
+. Increment
+
+. Decrement */
+
+//Artimetic Operators
+let a = 5;
+let b = 2;
+
+console. log ("a = ", a, " & b = ", b) ;
+console. log("a + b = ", a + b) ;
+console. log("a - b = ", a - b) ;
+console. log("a * b =", a * b);
+console. log("a / b =", a / b) ;
+console. log("a % b =", a % b) ;
+console. log("a ** b =", a ** b) ; // give me 25 ( 5^2 ) 
+
+// unary Operator
+// a++ = a+1
+// a-- = a-1 
+
+let a = 5;
+let b = 2;
+
+console. log("a = ", a, " & b = ", b) ;
+a--;
+
+console. log ("a = ", a); // 4
+
+//Asignment Operators
+
+let a = 5;
+let b = 2;
+
+a += 4; // a = a + 4
+console. log ("a = ", a); //9
+
+I
