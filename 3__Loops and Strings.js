d4c628b3781d73608dccbda8930e87ab3d69fd92
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2024-10-19 22:14:50 +0600

    Create 3__Loops and Strings.js

---
+++ 3__Loops and Strings.js
@@ -0,0 +1,14 @@
+// for Loop
+
+for (let i = 1; i <= 5; i++) {
+
+console.log("Shoeb");
+
+  
+
+let sum = 0 ;
+for (let i = 1; i <= 5; i++) {
+ sum = sum + i;
+}
+console.log(sum);
+console.log('loop done')
