c1a84d7a728205aeca8f951d15a2e32c94ed5aba
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2024-11-17 20:30:57 +0600

    Update 3__Loops and Strings.js

--- 3__Loops and Strings.js
+++ 3__Loops and Strings.js
@@ -80,3 +80,11 @@ while(userNum != gameNum) {
 }
 console.log("congo You enter the right Number");
 
+  let obj = {
+item: "pen",
+price: 10,
+};
+
+let output = `the cost of ${obj. item} is ${obj.price} rupees';
+console.log(output);
+// console.log("the cost of", obj.item, "is", obj.price, "rupees");
