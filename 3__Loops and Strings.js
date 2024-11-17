8657b66a96003861c63bea11877aef1037e342b1
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2024-11-17 20:32:40 +0600

    Update 3__Loops and Strings.js

--- 3__Loops and Strings.js
+++ 3__Loops and Strings.js
@@ -85,6 +85,8 @@ item: "pen",
 price: 10,
 };
 
-let output = `the cost of ${obj. item} is ${obj.price} rupees';
+let output = `the cost of ${obj. item} is ${obj.price} rupees`;
 console.log(output);
 // console.log("the cost of", obj.item, "is", obj.price, "rupees");
+
+
