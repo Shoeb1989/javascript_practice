cddd0166080fbdc1cb2e2557774c13dff4f3238b
Md Shoeb Sikder Pappu <shoebsikder1@gmail.com>
2025-01-25 17:05:10 +0600

    Update 4__Arrays.js

--- 4__Arrays.js
+++ 4__Arrays.js
@@ -5,3 +5,11 @@ console. log(marks. length);
 
 const cars = ["Saab", "Volvo", "BMW"];
 document.getElementById("demo").innerHTML = cars;
+
+
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+const iterator = colors.keys();
+for (const key of iterator) {
+  console.log(`${key}: ${colors[key]}`);
+}
